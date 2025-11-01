ea3cc138781ebbff822fecbad55a0fab9eacfeee
Khyojae <khjae201@gmail.com>
2025-11-01 23:17:52 +0900

    inital commit

---
+++ DOCKin/src/main/java/com/example/DOCKin/DocKinApplication.java
@@ -0,0 +1,13 @@
+package com.example.DOCKin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocKinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocKinApplication.class, args);
+	}
+
+}
---
+++ DOCKin/src/test/java/com/example/DOCKin/DocKinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DOCKin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocKinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
